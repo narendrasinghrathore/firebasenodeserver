ee420fa7f27bd7c0b3b80127663c60f223e370ed
narendrasinghrathore <rathoreofficialemail@gmail.com>
2020-06-22 10:33:02 +0530

    Node-Express server setup.

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require("firebase-functions");
+const express = require("express");
+
+const app = express();
+app.get("/title", (req, res) => {
+  res.send(`${Date.now()}`);
+});
+// How to cache data
+app.get("/title-cache", (req, res) => {
+  /**
+   * Using public, we can cache the content to Server, if we use private we can only store on user browser.
+   * max-age: How long we can cache the content on user browser.
+   * s-maxage: How long we can cache the content on server.
+   *
+   */
+  res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+  res.send(`${Date.now()}`);
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.app = functions.https.onRequest(app);
