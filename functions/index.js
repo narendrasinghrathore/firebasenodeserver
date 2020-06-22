d8f8750ae80f20795701d9c8bea36ab24574e2fc
narendrasinghrathore <rathoreofficialemail@gmail.com>
2020-06-22 10:39:00 +0530

    comments

--- functions/index.js
+++ functions/index.js
@@ -5,14 +5,13 @@ const app = express();
 app.get("/title", (req, res) => {
   res.send(`${Date.now()}`);
 });
-// How to cache data
+/**
+ * Using public, we can cache the content to Server, if we use private we can only store on user browser.
+ * max-age: How long we can cache the content on user browser.
+ * s-maxage: How long we can cache the content on server.
+ *
+ */
 app.get("/title-cache", (req, res) => {
-  /**
-   * Using public, we can cache the content to Server, if we use private we can only store on user browser.
-   * max-age: How long we can cache the content on user browser.
-   * s-maxage: How long we can cache the content on server.
-   *
-   */
   res.set("Cache-Control", "public, max-age=300, s-maxage=600");
   res.send(`${Date.now()}`);
 });
